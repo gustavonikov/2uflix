65525cf90c88daa4d9fd151f051c75603e387177
Gustavo <gustavonikov@gmail.com>
2021-06-13 22:00:36 -0300

    update: eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,39 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    //'prettier'
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es6: true,
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    //'prettier'
-  ],
-  rules: {
-    'linebreak-style': ['off'],
-    'object-curly-newline': ['off'],
-    'react/jsx-indent': ['off'],
-    'react/jsx-indent-props': ['off'],
-    'indent': ['error', 4],
-    'no-else-return': ['error',{allowElseIf: true}],
-    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
-    'react/jsx-one-expression-per-line': ['off'],
-    'react/no-array-index-key': ['off'],
-    'no-console': ['off'],
-    //"react/prop-types": ['off']
-    //'prettier/prettier': 'error'
-  },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+    // 'prettier'
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+    // 'prettier'
+    ],
+    rules: {
+        'linebreak-style': ['off'],
+        'object-curly-newline': ['off'],
+        'react/jsx-indent': ['off'],
+        'react/jsx-indent-props': ['off'],
+        indent: ['warn', 4],
+        'no-else-return': ['error', { allowElseIf: true }],
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+        'react/jsx-one-expression-per-line': ['off'],
+        'react/no-array-index-key': ['off'],
+        'no-console': ['off'],
+        'no-unused-vars': 'warn',
+    },
 };
